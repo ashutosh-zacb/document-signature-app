647a590d021ff8fd9b2daef52acb91a86339d529
Ashutosh <ashutoshzacb@gmail.com>
2026-06-11 19:09:49 +0530

    Day 3: PDF upload system completed

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,18 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
 from schemas.user import UserRegister, UserLogin
 import bcrypt
+import os
+import shutil
 
 app = FastAPI()
 
 users = []
+documents = []
+
+UPLOAD_FOLDER = "uploads"
+
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
 
 
 @app.get("/")
@@ -32,15 +40,39 @@ def register(user: UserRegister):
 
 @app.post("/api/auth/login")
 def login(user: UserLogin):
-
     for existing_user in users:
-
         if existing_user["email"] == user.email:
-
-            if bcrypt.checkpw(
-                user.password.encode("utf-8"),
-                existing_user["password"]
-            ):
+            if bcrypt.checkpw(user.password.encode("utf-8"), existing_user["password"]):
                 return {"message": "Login successful"}
 
     return {"message": "Invalid email or password"}
+
+
+@app.post("/api/docs/upload")
+def upload_document(file: UploadFile = File(...)):
+    if not file.filename.endswith(".pdf"):
+        return {"error": "Only PDF files are allowed"}
+
+    file_path = os.path.join(UPLOAD_FOLDER, file.filename)
+
+    with open(file_path, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
+    document = {
+        "id": len(documents) + 1,
+        "filename": file.filename,
+        "path": file_path,
+        "status": "uploaded"
+    }
+
+    documents.append(document)
+
+    return {
+        "message": "PDF uploaded successfully",
+        "document": document
+    }
+
+
+@app.get("/api/docs")
+def get_documents():
+    return documents
